7aaaa9945ac5380c2b680df8ed53d3012a46d8ca
jackob <jackobwu@gmail.com>
2022-09-27 21:06:54 +0800

    Beta

--- app/src/main/java/com/biling/huaming/MainActivity.kt
+++ app/src/main/java/com/biling/huaming/MainActivity.kt
@@ -1,27 +1,37 @@
 package com.biling.huaming
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.biling.huaming.ui.theme.HuamingTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -118,7 +128,75 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier) {
-    Text(text = "Hello", modifier.padding(50.dp))
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        // User's image
+        Image(
+            modifier = Modifier
+                .size(72.dp)
+                .clip(shape = CircleShape),
+            painter = painterResource(id = R.drawable.fc2_nature_meditations),
+            contentDescription = "Your Image"
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(weight = 3f, fill = false)
+                    .padding(start = 16.dp)
+            ) {
+
+                // User's name
+                Text(
+                    text = "Victoria Steele",
+                    style = TextStyle(
+                        fontSize = 22.sp,
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                // User's email
+                Text(
+                    text = "email123@email.com",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = androidx.compose.ui.graphics.Color.Gray,
+                        letterSpacing = (0.8).sp
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+
+            // Edit button
+            IconButton(
+                modifier = Modifier
+                    .weight(weight = 1f, fill = false),
+                onClick = { }
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Outlined.Edit,
+                    contentDescription = "Edit Details",
+                    tint = MaterialTheme.colors.primary
+                )
+            }
+
+        }
+    }
+
 }
 
 @Composable
@@ -144,16 +222,22 @@ fun NewsCard( modifier: Modifier = Modifier ) {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun NewsCardPreview() {
     NewsCard()
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
     HomeScreen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
 }
 
 @Preview(showBackground = true)
